4c6cf82fc5d095f2c2170fde9d81be47ef5f2041
Benjamin Troller <bctroller@gmail.com>
2019-01-12 19:15:40 -0800

    Memory class now loads OS by default

--- LC3 Simulator/Registers.swift
+++ LC3 Simulator/Registers.swift
@@ -8,14 +8,24 @@
 
 import Foundation
 
-class Registers : NSObject {
-    var pc : UInt16 = 0
+class Registers {
+    var pc : UInt16 = 0x3000
     var ir: UInt16 = 0
     var psr : UInt16 = 0
     var cc : UInt16 = 0
     var r : [UInt16] = [UInt16].init(repeating: 0, count: 8)
+    // Could use scheme like this to access each register individually and safely, but will take more room
+    // definitely convenient to have an array for main registers
+//    var r0 : UInt16 {
+//        get {
+//            return r[0]
+//        }
+//        set {
+//            r[0] = newValue
+//        }
+//    }
     
-    override init() {
-        super.init()
-    }
+//    override init() {
+//        super.init()
+//    }
 }
